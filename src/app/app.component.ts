555e6b5782faa6902278c711957177b9a9a029a4
Abinaya Bagavan (Digital) <AB20024003@wipro.com>
2018-12-07 16:10:44 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,22 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  template: `
+  <h1>{{title}}</h1>
+  <h2>Learning the {{concept}}</h2>
+  <h2>{{getString()}}</h2>
+  <h2>{{'This concept is '+getString()}}</h2>
+  <h4 title={{title}}>Move here to see the title</h4>
+  `,
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  title= 'Angular project';
+  concept='Interpolation';
+  returnString='Component and template ';
+  getString(): string{
+    return this.returnString;
+  }
+
+}
+
+
